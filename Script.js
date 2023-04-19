cead994815183d4ba20c2842631f1d9f997cd8d7
Gustavo <gustavo.rlsilva07@gmail.com>
2023-04-19 18:40:48 -0300

    trabalhando com incluides

---
+++ Script.js
@@ -0,0 +1,9 @@
+let fruits = ["banana", "laranja", "maça", "pêra"];
+
+if(fruits.includes('uva')) {
+    console.log("tem uva")
+}else {
+    console.log("não tem uva ")
+}
+
+/* includes vai te retornar se tal elemento existe dentro do array*/
