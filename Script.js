0d3d3066547619c5d7a2c95661da4cb393212922
Gustavo <gustavo.rlsilva07@gmail.com>
2023-05-04 00:04:02 -0300

    eventos de clique com query

--- Script.js
+++ Script.js
@@ -1,7 +1,6 @@
-let elemento = document.getElementsByTagName("h1") //pode ser salvo em uma variável (caso seu uso seja feito diretamente pelo console, não precisa usar uma variável e console.log)
-
-console.log(elemento[1]) //aqui ele ta chamando o segundo h1, pq começa a contar a partir do 0
-
 function clicou() {
-    alert("Clicou no botão")
+    console.log("Clicou no botão");
 }
+
+let botao = document.querySelector('.botao'); //selecionando o elemento
+botao.addEventListener("click", clicou()); //addEventListener = escutador de eventos
