0cefa4fdd4cdd20f2e361254ce37d44b8ad3a4df
Gustavo <gustavo.rlsilva07@gmail.com>
2023-05-04 22:51:02 -0300

    manipulando um elemento dentro de outro com innerHTML e children

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 function clicou() {
-    const teste = document.querySelector("#teste");
-    //console.log(teste.children); //vai mostrar o que tem dentro da div, mas não vai mostrar o li, pq o ul é filho da div, mas o li nao, a forma correta seria:
-    console.log(teste.children[0].children) //o children retorna uma array 
+    const teste = document.querySelector("#teste"); //selecionando a div
+    const ul = teste.querySelector('ul'); //selecionando o ul
+
+    ul.children[0].innerHTML = "item <strong>alterado</strong>" //pegando o primeiro li e modificando seu texto
 }
