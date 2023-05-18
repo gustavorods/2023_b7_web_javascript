ff4ed428c32d4f645f640c42f25a713b0f363d7d
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-05-18 19:41:29 -0300

    .appendChild

--- Script.js
+++ Script.js
@@ -2,5 +2,8 @@ function clicou() {
     const teste = document.querySelector("#teste");
     const ul = teste.querySelector('ul');
 
-    ul.append("<li>Item adicionado</li>")
+   let novoli = document.createElement("li"); //aqui ele cria um novo elemento <li>, mas pode ser h1, etc.
+    novoli.innerText = "item adicionado";   //aquu ele "decide" que o novoli (nome da variavel) vai receber um valor de texto, por isso "text"
+
+    ul.appendChild(novoli) //aqui o appendChild adiciona o novoli
 }
