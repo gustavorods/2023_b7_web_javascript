7caf0f21433d5a842b7eb67a91457c921d00fdd6
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-06-22 23:26:29 -0300

    trabalhando com ações

--- Script.js
+++ Script.js
@@ -1,31 +1,31 @@
-//esta sendo criado o template agr
-
     class Person {
 
-    age = 0; //definindo o valor de age
+    //variaveis
+    age = 0;
+    steps = 0;
 
-    constructor(name, age) {
+
+    //construtor
+    constructor(name) {
         this.name = name;
     }
+
+
+    //ações
+    takeAstep() {
+        this.steps++;
+    }
+
 }
 
 
-//para criar as pessaos, vc vai ter que instânciar uma classe (em outras palavras, pegar o template anterior e adicionar os valores )
 
 let p1 = new Person("Gustavo");
 let p2 = new Person("Maria");
 let p3 = new Person("Pedro");
 
-console.log(p1.age); 
-console.log(p2.age); 
-console.log(p3.age);
+p1.takeAstep();
+p1.takeAstep();
 
-/*todos vão ter 0 anos, mas vc pode adicionar, você pode colocar a idade com: 
-
-exemplo: p1.age = 30 
-
-obs:age é uma propriedade que exxites, mas não foi preenchida no construtor, porém existe
-
-
-*/
-
+console.log(`Passos de ${p1.name}: ${p1.steps}`)
+console.log(`Passos de ${p2.name}: ${p2.steps}`)
