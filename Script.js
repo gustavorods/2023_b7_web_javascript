0c522e9bf2afdf049702e0f0d962a6c7e17c4e0d
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-06-27 23:42:48 -0300

    setter e getter

--- Script.js
+++ Script.js
@@ -18,11 +18,17 @@
 
 
     get age() {
-        return this._age;
-    }
+        return this._age; //é necessário renomear o age, por isso o "_age" (os nomes tem que ser diferente )
+    } //vai retornar o valor que eu quero 
 
     get fullname() {
-        return `${this.firstName} ${this.lastName}`
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set age(x) {
+        if(typeof x == "number") {
+            this._age = x; //x armazena temporiariamente 
+        }
     }
 
 }
@@ -33,4 +39,9 @@ let p1 = new Person("Gustavo", "Silva");
 let p2 = new Person("Maria");
 let p3 = new Person("Pedro");
 
+
+p1.age = 10;
+p2.age = 1
+
 console.log(`${p1.fullname} tem ${p1.age} anos`)
+console.log(`${p2.fullname} tem ${p2.age} anos`)
