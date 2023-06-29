59105f21a68469d0027de10dd9cfdc3ef0489d30
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-06-29 19:01:04 -0300

    trabalhando com static

--- Script.js
+++ Script.js
@@ -1,35 +1,25 @@
 class Person {
 
+    static hands = 2;
     age = 0;
 
     constructor(name) {
         this.name = name;
-
-
     }
 
     sayHi() {
-        console.log(`${this.name} diz OI `)
-    } 
+        console.log(`1° forma: Oi ${this.name}, eu tenho ${Person.hands} mãos!`)
 
-}
-
-
-class Student extends Person {
-
-    
-    constructor(name, id) {
-        super(name);
-        this.id = id;
-    }
-
-    sayHello() {
-        super.sayHi();
-        console.log("tudo bem?"); 
+        console.log("------------------------------------") //quebrar linha
     }
 }
 
+let p1 = new Person ("gu");
+let p2 = new Person ("Lo");
 
-let p1 = new Student("Pedro", 11123)
+p1.sayHi();
 
-p1.sayHello(); 
+Person.hands = 1
+
+console.log(`2° forma: Oi ${p1.name}, eu tenho ${Person.hands} mãos!`)
+console.log(`2° forma: Oi ${p2.name}, eu tenho ${Person.hands} mãos!`)
