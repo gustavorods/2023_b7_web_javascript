7381935ce79658eca69126b2783e515c36defc1f
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-07-15 20:21:47 -0300

    trabalhando com o conceito de factory

--- Script.js
+++ Script.js
@@ -1,25 +1,20 @@
 class Person {
-
-    static hands = 2;
-    age = 0;
+    age = 0; 
 
     constructor(name) {
         this.name = name;
     }
-
-    sayHi() {
-        console.log(`1° forma: Oi ${this.name}, eu tenho ${Person.hands} mãos!`)
-
-        console.log("------------------------------------") //quebrar linha
-    }
 }
 
-let p1 = new Person ("gu");
-let p2 = new Person ("Lo");
+function createPerson(name, age) {
+    let p = new Person(name);
+    p.age = age;    
+    return p;
+}
 
-p1.sayHi();
+let p1 = createPerson("gu", 15);
 
-Person.hands = 1
+console.log(`${p1.name} tem ${p1.age} anos!`);
 
-console.log(`2° forma: Oi ${p1.name}, eu tenho ${Person.hands} mãos!`)
-console.log(`2° forma: Oi ${p2.name}, eu tenho ${Person.hands} mãos!`)
+console.log(p1)
+
