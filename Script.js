58288c0d0169503c14ab3b4ee25b2a2d1f98f0b5
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-08-01 21:38:11 -0300

    Programação funciona: this, instancia

--- Script.js
+++ Script.js
@@ -1,20 +1,36 @@
-class Person {
-    age = 0; 
-
-    constructor(name) {
-        this.name = name;
+function createPerson(nome, sobrenome, idade) {
+    return {
+        nome,
+        sobrenome,
+        idade,
+        NomeInteiro() {
+            return `${this.nome} ${this.sobrenome}`
+        }
     }
 }
 
-function createPerson(name, age) {
-    let p = new Person(name);
-    p.age = age;    
-    return p;
+let person1 = createPerson("Gustavo", "Rodrigues", "16");
+let person2 = createPerson("Pedro", "Alves", "32");
+
+console.log("Olá", person1.NomeInteiro());
+
+/* 
+Erro para não cometer: 
+
+function createPerson(nome, sobrenome, idade) {
+    return {
+        nome,
+        sobrenome,
+        idade,
+        NomeInteiro: () => {
+            return `${this.nome} ${this.sobrenome}`
+        }
+    }
 }
 
-let p1 = createPerson("gu", 15);
+Motivo: 
+Esse arrow function que eu criei é uma função anonima (que não possui nome), ou seja, é independente,
+que significa que ela não tem acesso as outras propriedades como nome e sobrenome.
 
-console.log(`${p1.name} tem ${p1.age} anos!`);
-
-console.log(p1)
-
+Ali só foi pego um função independente e foi jogada numa propriedade(nome inteiro)
+*/
