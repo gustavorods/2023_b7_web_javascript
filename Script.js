7c5e80d2a6ef03aac3115d83c1ea09e841f68a21
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-08-01 22:29:53 -0300

    herança em programação funcionar

--- Script.js
+++ Script.js
@@ -1,36 +1,19 @@
-function createPerson(nome, sobrenome, idade) {
-    return {
-        nome,
-        sobrenome,
-        idade,
-        NomeInteiro() {
-            return `${this.nome} ${this.sobrenome}`
-        }
-    }
+const defaultUser = {
+    name: "", 
+    email: "", 
+    level: 1, 
+
 }
 
-let person1 = createPerson("Gustavo", "Rodrigues", "16");
-let person2 = createPerson("Pedro", "Alves", "32");
-
-console.log("Olá", person1.NomeInteiro());
-
-/* 
-Erro para não cometer: 
-
-function createPerson(nome, sobrenome, idade) {
-    return {
-        nome,
-        sobrenome,
-        idade,
-        NomeInteiro: () => {
-            return `${this.nome} ${this.sobrenome}`
-        }
-    }
+let user1 = {
+    ...defaultUser, //herdando as especificações de "defaultUSer"
+    name: "Gustavo", //depois de herdar uma outra função, você pode adicionar mais coisa ou substituir as coisas, como no caso do "name".
+    email: "undefield@bla...",
 }
 
-Motivo: 
-Esse arrow function que eu criei é uma função anonima (que não possui nome), ou seja, é independente,
-que significa que ela não tem acesso as outras propriedades como nome e sobrenome.
-
-Ali só foi pego um função independente e foi jogada numa propriedade(nome inteiro)
-*/
+let adm1 = {
+    ...defaultUser,
+    name: "Gustavo adm",
+    email: "ADM@bla...",
+    level: 2,
+}
