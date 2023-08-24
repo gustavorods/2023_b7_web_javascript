46ca20e0166e9f4f6eeb0058736983eb06a6c19e
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-08-23 23:03:58 -0300

    tipos de status

--- Script.js
+++ Script.js
@@ -1,22 +1,18 @@
-function clicou() 
-{
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => {
-            return response.json();     
-        })
-        .then((data) => {
-            alert(`O título do primeiro post é: ${data[0].title}`);
-        })
-        .catch((erro) => {
-            alert("Algo deu errado durante a requisição, abra o console para verificar"); // Alerta que aparecera pro usuário
-            console.log(`O erro foi o seguinte: ${erro}`);
-        })
-        .finally(() => {
-            alert("Deu tudo certo ou tudo errado? risos.")
-        })
-        
-        
+    function clicou() 
+    {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                console.log(`Status: ${response.status}`) 
+                return response.json();     
+            })
+            .then((data) => {
+                alert(`O título do primeiro post é: ${data[0].title}`);
+            })
+            .catch((erro) => {
+                alert("Algo deu errado durante a requisição, abra o console para verificar"); // Alerta que aparecera pro usuário
+                console.log(`O erro foi o seguinte: ${erro}`);
+            });
 
-}
+    }
 
-document.querySelector('#botao').addEventListener('click', clicou);
+    document.querySelector('#botao').addEventListener('click', clicou);
