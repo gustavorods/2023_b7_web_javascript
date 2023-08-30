ab57614e3ad90edc3d420e1a6c828b48d9f23474
gustavorods <gustavorodrigues@pop-os.localdomain>
2023-08-29 22:37:55 -0300

    requisição mais facil + criando próprio post

--- Script.js
+++ Script.js
@@ -1,43 +1,41 @@
-    function clicou() 
-    {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then((response) => {
-                console.log(`Status: ${response.status}`) 
-                return response.json();     
-            })
-            .then((data) => {
-                alert(`O título do primeiro post é: ${data[0].title}`);
-            })
-            .catch((erro) => {
-                alert("Algo deu errado durante a requisição, abra o console para verificar"); // Alerta que aparecera pro usuário
-                console.log(`O erro foi o seguinte: ${erro}`);
-            });
-    }
-    function Inserir() 
-    {
-        fetch(
-            'https://jsonplaceholder.typicode.com/posts',
-            {
-                method: "POST", // Método de envio da requisição
-                headers: {
-                    'Content-Type': 'application/json', // A forma que eu vou enviar o body (conteúdo) do posto é atravez de um json
-                },
-                body: JSON.stringify({ // JSON.stringify = é usado para converter o objeto JavaScript em uma string JSON válida antes de enviá-lo como corpo da solicitação. 
-                    title: 'Título de teste',
-                    body: 'Título de teste',
-                    userId: 2 // Autor / usuario
-                    // Quando fizer a requisição, automaticamente ele vai criar um ID pra ela
-                })   
-            }
-        )
-        .then((response) => {
-            return response.json(); // Transformando o json da resposta em um objeto
-         })
-         .then((data) => {
-             console.log(data); // NÃO ESQUECE DE COLOCAR NO NOTION A FUNÇÕES DA ABA NETWORK DO DEV TOOLS (AULA DO BONIEKY)
-         })
-    }
+// TEMP: antes do nome da função que retorna uma promise (await) 09:28
 
-    document.querySelector('#botao').addEventListener('click', clicou);
-    document.querySelector('#inserir').addEventListener('click', Inserir); // Botão de inserir um Post
-            
+async function clicou() {
+    try {
+        let req = await fetch('https://jsonplaceholder.typicode.com/posts'); // Vai esperar a requisição 
+        if (!req.ok) {
+            throw new Error(`Erro na requisição: ${req.status}`);
+        }
+
+        let convert = await req.json(); // Vai esperar a conversão | usar await aqui não é estritamente necessário, mas é uma boa prática ;)
+
+        alert(`O título do primeiro post é: ${convert[0].title}`);
+        alert("OPAA");
+    } catch (erro) {
+        console.log(`Ocorreu um erro: ${erro}`);
+    }
+}
+
+async function Inserir() {
+    let req = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: 'Título de teste',
+                body: 'Título de teste',
+                userId: 2
+            })
+        }
+    );
+
+    let convert = await req.json();
+
+    console.log(`O título do post é: ${convert.title}`);
+}
+
+document.querySelector('#botao').addEventListener('click', clicou);
+document.querySelector('#inserir').addEventListener('click', Inserir);
