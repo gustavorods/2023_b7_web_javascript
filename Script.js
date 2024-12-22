31707b16e1be198fe2c340e7d9de7db019df82c4
gustavorods <gustavo.rlsilva07@gmail.com>
2024-12-22 11:59:26 -0300

    Operador Rest (receber array como parâmetro )

--- Script.js
+++ Script.js
@@ -1,47 +1,5 @@
-// TEMP: por que "/post" o método não é GET?
-// TEMP: get puxando id
-
-async function clicou() {
-    try {
-        let req = await fetch('https://jsonplaceholder.typicode.com/posts'); // Vai esperar a requisição 
-        if (!req.ok) { // Vende se a requisição deu errado 
-            throw new Error(`Erro na requisição: ${req.status}`);
-        }
-        
-        let convert = await req.json(); /* Usar "await" aqui é recomendado em contexto assíncrono. Sem ele, a função continua antes da conclusão da chamada, 
-        causando resultados imprevisíveis, pois "req.json()" é uma promessa. Com "await", o código espera pela resolução da promessa, garantindo que "convert" 
-        seja definido corretamente antes de prosseguir, vital em ambientes assíncronos. */
-
-        alert(`O título do primeiro post é: ${convert[0].title}`);
-        alert("OPAA");
-    } 
-    catch (error) {
-        console.error("Ocorreu um erro:", error);
-    }
+function adicionar(...numeros) {
+    console.log(numeros);
 }
 
-
-async function Inserir() {
-    let req = await fetch(
-        'https://jsonplaceholder.typicode.com/posts',
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                title: 'Título de teste',
-                body: 'Título de teste',
-                userId: 2
-            })
-        }
-    );
-
-    let convert = await req.json();
-
-    console.log(`O título do post é: ${convert.title}`);
-    console.log(`oi`);
-}
-
-document.querySelector('#botao').addEventListener('click', clicou);
-document.querySelector('#inserir').addEventListener('click', Inserir);
+adicionar(1,2,4,); // (3) [1, 2, 4]
