6186809039204a08f31811d20bd97c5256f6b6d5
gustavorods <gustavo.rlsilva07@gmail.com>
2024-12-24 10:09:25 -0300

    feat part 2 - projeto de blog finalizado

--- Script.js
+++ Script.js
@@ -7,9 +7,21 @@ function loadPosts() {
             return resultado.json(); // retorna uma promises, ou seja, precisa de um "then"
         })
         .then(function(json) {
-            document.getElementById("posts").innerHTML = json.length + "posts";
+            montarBlog(json);
         })
         .catch(function(error) {
             console.error("Não foi possível fazer a requisição \n " + error);
         })
+}
+
+function montarBlog(lista) {
+    let html = '';
+
+    for(let i in lista) {
+        html += `<h3>${lista[i].title}</h3>`;
+        html += `${lista[i].body} <br/>`;
+        html += `<hr/>`;
+    }
+
+    document.getElementById("posts").innerHTML = html;
 }
