65bd71521ae9e4902be8954335c88018e0c1d64e
gustavorods <gustavo.rlsilva07@gmail.com>
2024-12-24 17:28:56 -0300

    Thumbnails com JS

--- Script.js
+++ Script.js
@@ -1,16 +1,12 @@
-async function enviar() {
-    let arquivo = document.getElementById("arquivo").files;
+function mostrar() {
+    let imagem = document.getElementById("imagem").files[0]; 
+    
+    let img = document.createElement('img'); // Cria dinamicamente um elemento <img>
+    img.src = URL.createObjectURL(imagem); // Define a URL temporária do arquivo selecionado como a fonte da imagem
+    img.width = 200; // Ajusta a largura da imagem para 200 pixels
 
-    let body = new FormData();
-    body.append('title', 'blabla');
-    body.append('arquivo', arquivo);
-
-    let req = await fetch("https://blablalba,com.br/upload", {
-        method: 'POST',
-        body: body, 
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    })
+    // Adiciona o elemento <img> criado ao elemento com id "area"
+    // Não usamos innerHTML aqui porque ele substitui todo o conteúdo existente e cria os elementos diretamente como texto HTML.
+    // Como estamos criando o elemento com createElement, o correto é usar um método que insira o nó diretamente na árvore DOM, como appendChild.
+    document.getElementById("area").appendChild(img);
 }
-
