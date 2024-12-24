9fed63085d59e8db9da13bbe71cfaf71dd9b93c1
gustavorods <gustavo.rlsilva07@gmail.com>
2024-12-24 09:55:50 -0300

    Fetch part 1

--- Script.js
+++ Script.js
@@ -1,34 +1,15 @@
-function pegarTemperatura() {
-    return new Promise(function(resolve, reject) {
-        console.log("Pegando temperatura...");
+function loadPosts() {
+    document.getElementById("posts").innerHTML = "Carregando...";
 
-        setTimeout(function() {
-            const sucesso = Math.random() > 0.5; // Simula um sucesso ou falha aleatório
-            if (sucesso) {
-                resolve('40 na sombra');
-            } else {
-                reject('Erro ao pegar a temperatura');
-            }
-        }, 2000);
-    });
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function(resultado) {
+            // transformar em JSON
+            return resultado.json(); // retorna uma promises, ou seja, precisa de um "then"
+        })
+        .then(function(json) {
+            document.getElementById("posts").innerHTML = json.length + "posts";
+        })
+        .catch(function(error) {
+            console.error("Não foi possível fazer a requisição \n " + error);
+        })
 }
-
-// USANDO A PROMISE
-console.log('Código antes!');
-
-let temp = pegarTemperatura();
-
-console.log("Código durante");
-
-temp.then(function(resultado) {
-    console.log("TEMPERATURA: " + resultado);
-}).catch(function(error) {
-    console.log("Eita, deu erro! Detalhes: " + error);
-});
-
-/*
-resolve: Passa o valor para o then.
-reject: Passa o valor para o catch.
- */
-
-console.log("Código depois");
